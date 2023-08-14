1bbc2e010caf2a032dd57f16239b3f12094d6c80
jeffigy <evangelistajeffy25@gmail.com>
2023-08-14 19:19:07 +0800

    modified establishment details page but not final yet

--- src/app/establishments/[...est_id]/layout.tsx
+++ src/app/establishments/[...est_id]/layout.tsx
@@ -1,27 +1,73 @@
 "use client";
-import { SimpleGrid, GridItem } from "@chakra-ui/react";
+import Header from "@/Components/Header";
+import {
+  SimpleGrid,
+  GridItem,
+  Box,
+  Text,
+  Flex,
+  Input,
+  Button,
+  InputGroup,
+  InputRightElement,
+  Icon,
+} from "@chakra-ui/react";
 import React from "react";
+import { Search } from "react-feather";
 
 type EstablishmentsLayoutProps = {
   children: React.ReactNode;
-  feeds: React.ReactNode;
-  patients: React.ReactNode;
-  schedules: React.ReactNode;
 };
 
 const EstablishmentsLayout: React.FC<EstablishmentsLayoutProps> = ({
   children,
-  feeds,
-  patients,
-  schedules,
 }) => {
   return (
     <>
+      <Header label="Establishment Details" />
       {children}
       <SimpleGrid columns={4} gap={"10px"}>
-        <GridItem>{patients}</GridItem>
-        <GridItem colSpan={2}>{feeds}</GridItem>
-        <GridItem>{schedules}</GridItem>
+        <GridItem colSpan={3}>asdf</GridItem>
+        <GridItem>
+          {" "}
+          <Box
+            mb={"10px"}
+            w={"full"}
+            p={"10px"}
+            border={"5px solid"}
+            borderColor={"brand.900"}
+            bgColor={"brand.100"}
+          >
+            <Text
+              overflow={"hidden"}
+              lineHeight={0.9}
+              fontSize={"24px"}
+              fontWeight={"bold"}
+            >
+              Establishment Name
+            </Text>
+            <Text>Address</Text>
+            <Text lineHeight={0.9}>Owner</Text>
+          </Box>
+          <Flex direction={"column"}>
+            <Flex align={"center"} justify={"space-between"}>
+              {" "}
+              <Text fontWeight={"semibold"}>Patients</Text>
+              <InputGroup w={"200px"} bgColor={"white"} size="sm">
+                <Input
+                  // border={"none"}
+                  pr="4.5rem"
+                  type="text"
+                  placeholder="Search"
+                  _placeholder={{ color: "gray.400" }}
+                />
+                <InputRightElement width="4.5rem">
+                  <Icon as={Search} color={"gray.400"} />
+                </InputRightElement>
+              </InputGroup>
+            </Flex>
+          </Flex>
+        </GridItem>
       </SimpleGrid>
     </>
   );
